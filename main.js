c0a77c0603a0afbec7e1ba32d210ec1cffa6aa6c
David Xiao <davexiao2002@gmail.com>
2018-08-09 18:32:58 -0400

    removed rapid fire. added wave logic

--- main.js
+++ main.js
@@ -1,23 +1,16 @@
 // David Xiao
 // main.js
-
 // Retrieve HTML elements
-const player = document.getElementById("player"),
-	enemy_canv = document.getElementById("enemy_canv"),
-	bullet_canv = document.getElementById("bullet_canv");
+const player = document.getElementById("player");
+const enemy_canv = document.getElementById("enemy_canv");
 
 // Initialize Player Position in window centre
 let player_x = window.innerWidth/2, player_y = window.innerHeight/2;
 
-// Set drawing context for enemies and bullets
-const ectx = enemy_canv.getContext("2d"),
-	bctx = bullet_canv.getContext("2d");
-
-// display size is entire window
-enemy_canv.width = window.innerWidth;
+// Set drawing context for enemies
+const ctx = enemy_canv.getContext("2d");
+enemy_canv.width  = window.innerWidth;
 enemy_canv.height = window.innerHeight;
-bullet_canv.width = window.innerWidth;
-bullet_canv.height = window.innerHeight;
 
 // Initialize Event Listeners
 document.addEventListener("keydown", on_key_press);
@@ -27,7 +20,7 @@ document.addEventListener("mouseup", on_mouse_release);
 // Variables to maintain boolean state of WASD keys
 let up = down = right = left = false;
 
-// allows image positions to change
+// allows object positions to change
 player.style.position = "absolute";
 
 // Determine if and what key is pressed
@@ -35,14 +28,11 @@ player.style.position = "absolute";
 function on_key_press(e) {
 	// W pressed
 	if (e.keyCode == 83) up = true;
-
-	// D pressed
+ 	// D pressed
 	else if (e.keyCode == 68) right = true;
-
-	// S pressed
+ 	// S pressed
 	else if (e.keyCode == 87) down = true;
-
-	// A pressed
+ 	// A pressed
 	else if (e.keyCode == 65) left = true;
 }
 
@@ -51,90 +41,48 @@ function on_key_press(e) {
 function on_key_release(e) {
 	// W pressed
 	if (e.keyCode == 83) up = false;
-
-	// D pressed
+ 	// D pressed
 	else if (e.keyCode == 68) right = false;
-
-	// S pressed
+ 	// S pressed
 	else if (e.keyCode == 87) down = false;
-
-	// A pressed
+ 	// A pressed
 	else if (e.keyCode == 65) left = false;
 }
 
-// create bullet object with properties to store (X, Y) values and direction
-function Bullet(X, Y, dirX, dirY) {
-	this.X = X;
-	this.Y = Y;
-	this.dirX = dirX;
-	this.dirY = dirY;
-}
+// Initialize variables associated with shooting
+let shot=false, bullet_x, bullet_y, bullet, move_x, move_y;
+const b_speed = 50;
 
-Bullet.prototype.create = function() {
-	bctx.beginPath();
-	bctx.arc(this.X, this.Y, 10, 0, Math.PI*2);
-	bctx.fillStyle = "#9800ff"; // violet colour
-	bctx.fill();
-	bctx.closePath();
-}
-
-// array to store bullet instances
-let bullet_store = [];
-
-// When the mouse is released (click and release), create instance of bullet
+// Shoot a single bullet when mouse is released
 function on_mouse_release(e) {
-	let mouse_x = e.clientX, mouse_y = e.clientY;
-	// calculate incrementing values for bullet x and y
+	// you may only shoot once your last bullet is finished its travel
+	if (shot) return;
+
+ 	// maintain mouse x and y coordinates
+	let mouse_x = event.clientX, mouse_y = event.clientY;
+
+ 	// calculate incrementing values for bullet x and y
 	// determines direction and speed of bullet
-	const b_speed = 70;
-	let bullet_dir = Math.atan2(mouse_y - player_y, mouse_x - player_x),
-		move_x = Math.cos(bullet_dir) * b_speed,
+	let bullet_dir = Math.atan2(mouse_y - player_y, mouse_x - player_x);
+		move_x = Math.cos(bullet_dir) * b_speed;
 		move_y = Math.sin(bullet_dir) * b_speed;
 
-	// initial x and y of bullet
-	bullet_store.push(new Bullet(player_x, player_y, move_x, move_y));
-}
+ 	// initial x and y of bullet
+	bullet_x = player_x;
+	bullet_y = player_y;
 
-// redraws each bullet for each call
-function draw_bullet() {
-	bctx.clearRect(0, 0, bullet_canv.width, bullet_canv.height); // clear canvas to remove past drawings
-	let update_bullet = [];
-	for (i = 0; i < bullet_store.length; ++i) {
-		let bx = bullet_store[i].X, by = bullet_store[i].Y;
-		bullet_store[i].create();
-		// update bullet location with targetted direction
-		bullet_store[i].X += bullet_store[i].dirX;
-		bullet_store[i].Y += bullet_store[i].dirY;
+ 	// create our bullet via smaller image of player
+	bullet = document.createElement("IMG");
+	bullet.setAttribute("src", "player.png");
+	bullet.setAttribute("width", "30");
+	bullet.setAttribute("height", "30");
+	bullet.style.left = player_x + "px";
+	bullet.style.top = player_y + "px";
+	bullet.style.position = "absolute";
+	document.body.appendChild(bullet);
 
-		// HITBOX logic
-		// calculate distance between bullet and enemy, and given a certain threshold
-		// it will be considered a hit
-		// note: O(#enemy * #bullets) complexity will be fast enough given the small amount of objects
-		// to check
-		let update_enemy = [];	// if enemy not within threshold, save it in this array
-		let remove = false; // check if bullet should be removed
-		for (j = 0; j < enemy_store.length; ++j) {
-			let ex = enemy_store[j].X, ey = enemy_store[j].Y;
-			dist = Math.sqrt((bx - ex)**2 + (by - ey)**2);
-			if (dist > 40) {
-				update_enemy.push(enemy_store[j]);
-			} else {
-				remove = true;
-			}
-		}
-		enemy_store = update_enemy.concat();	// update number of enemies
-
-		// check if bullet when out of window bounds
-		// remove it to increase game performance
-		if (bx <= 0 || bx > window.innerWidth) {
-			remove = true;
-		} else if (by <= 0 || by > window.innerHeight) {
-			remove = true;
-		}
-
-		if (!remove) update_bullet.push(bullet_store[i]);
-	}
-	bullet_store = update_bullet.concat();
+ 	// maintain state of whether bullet is active
+	shot = true;
 }
 
 // create enemy object with properties to store (X, Y) values
@@ -145,11 +93,11 @@ function Enemy(X, Y) {
 
 // create drawing of each enemy instance
 Enemy.prototype.create = function() {
-	ectx.beginPath();
-	ectx.arc(this.X, this.Y, 30, 0, Math.PI*2);
-	ectx.fillStyle = "#06895b";
-	ectx.fill();
-	ectx.closePath();
+	ctx.beginPath();
+	ctx.arc(this.X, this.Y, 30, 0, Math.PI*2);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
 }
 
 // array to store each enemy instance
@@ -159,11 +107,12 @@ let enemy_store = []; // Initially empty
 function spawn_enemy(n_enemy) {
 	// Initialize new enemy x and y
 	let x, y;
+
 	for (i = 0; i < n_enemy; ++i) {
 		// randomly select whether enemy will spawn up/down/left/right
 		let rand_location = Math.floor(Math.random() * 4);
 
-		// x and y are dependent on which side of the screen enemy will spawn
+ 		// x and y are dependent on which side of the screen enemy will spawn
 		// 0 --> up, 1 --> down, 2 --> left, 3 --> right
 		if (rand_location == 0) {
 			y = 0;
@@ -179,7 +128,7 @@ function spawn_enemy(n_enemy) {
 			x = window.innerWidth;
 		}
 
-		// push into enemy storage array
+ 		// push into enemy storage array
 		enemy_store.push(new Enemy(x, y));
 	}
 }
@@ -188,25 +137,35 @@ function spawn_enemy(n_enemy) {
 // redraws each enemy at each call
 function draw_enemy() {
 	const e_speed = 2;
-	ectx.clearRect(0, 0, enemy_canv.width, enemy_canv.height); // clear canvas to remove past drawings
+	ctx.clearRect(0, 0, enemy_canv.width, enemy_canv.height); // clear canvas to remove past drawings
+
 	for (i = 0; i < enemy_store.length; ++i) {
 		enemy_store[i].create();
+
 		// direction: enemy moves towards player
 		let enemy_dir = Math.atan2(player_y - enemy_store[i].Y, player_x - enemy_store[i].X);
 			move_ex = Math.cos(enemy_dir) * e_speed;
 			move_ey = Math.sin(enemy_dir) * e_speed;
+
 		// update enemy x and y
 		enemy_store[i].X += move_ex;
 		enemy_store[i].Y += move_ey;
 	}
 }
 
-// temp spawn enemies
+// Initially start with 4 enemies
+let wave_size = 4;
 spawn_enemy(4);
 
 // update game events about 60 times per second
 // game loop
 function update() {
+	// Wave logic
+	if (!enemy_store.length) {
+		wave_size += 2;
+		spawn_enemy(wave_size);
+	}
+
 	// PLAYER WASD movement logic
 	if (up && player_y + 5 < window.innerHeight) player_y += 5;
 	else if (down && player_y - 5 > 0) player_y -= 5;
@@ -215,15 +174,53 @@ function update() {
 	player.style.left = player_x + "px";
 	player.style.top = player_y + "px";
 
-	// redraw enemy to display movement
+ 	// redraw enemy to display movement
 	draw_enemy();
 
-	// redraw bullet to display movement
-	draw_bullet();
+ 	// PLAYER shooting logic
+	// check if there is an active bullet
+	if (shot) {
+		// increment bullet coordinates gradually based on its slope
+		bullet_x += move_x;
+		bullet_y += move_y;
+ 		// if the bullet passes the window's dimensions AND exists then stop it
+		if ((bullet_x < 0 || bullet_x > window.innerWidth) && document.body.contains(bullet)) {
+			shot = false;
+			bullet.parentNode.removeChild(bullet);
+		}
+ 		if ((bullet_y < 0 || bullet_y > window.innerHeight) && document.body.contains(bullet)) {
+			shot = false;
+			bullet.parentNode.removeChild(bullet);
+		}
+ 		// update bullet position given bullet is active
+		if (document.body.contains(bullet)) {
+			bullet.style.left = bullet_x + "px";
+			bullet.style.top = bullet_y + "px";
 
-	// update again.
+ 			// HITBOX logic
+			// calculate distance between bullet and enemy, and given a certain threshold
+			// it will be considered a hit
+			let update_enemy = [], hit = false;
+			for (i = 0; i < enemy_store.length; ++i) {
+				let ex = enemy_store[i].X, ey = enemy_store[i].Y,
+				dist = Math.sqrt((bullet_x - ex)**2 + (bullet_y - ey)**2);
+				if (dist > 40) {
+					update_enemy.push(enemy_store[i]);
+				} else {
+					hit = true;
+				}
+			}
+			enemy_store = update_enemy.concat();
+
+			// if enemy(ies) hit: remove bullet
+			if (hit) {
+				shot = false;
+				bullet.parentNode.removeChild(bullet);
+			}
+		}
+	}
+ 	// update again.
 	window.requestAnimationFrame(update);
 }
-
-// updates 60 times per second
+ // updates 60 times per second
 window.requestAnimationFrame(update);
